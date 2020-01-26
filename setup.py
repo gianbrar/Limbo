70b3b1a24c4416d2dfe707cef83007393ec13fa1
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2020-01-25 21:01:52 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-setup = input("Start client or server? (S/C)")
-if setup.lower() == "s":
-  print("Starting server")
-  system("python3 limbo_server.py")
-elif setup.lower() == "c":
-  print("Starting client")
-  system("python3 limbo_client.py")
