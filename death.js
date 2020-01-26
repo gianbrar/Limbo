ef436f821c23c4544479865c471eaeca519b4312
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2020-01-26 04:05:11 -0500

    Update death.js

--- death.js
+++ death.js
@@ -1,7 +1,47 @@
-var randomVar = Math.floor((Math.random() * 50) + 1);
 var killer = "";
-var death = "";
-if (randomVar === 1) {
-    killer = "Yakuza";
+var name = "";
+switch (Math.floor((Math.random() * 50) + 1)) {
+    case 1:
+        killer = "a Yakuza Boss";
+        break;
+    case 2:
+        switch (Math.floor((Math.random() * 11) + 1)) {
+            case 1:
+                name = "Ray";
+                break;
+            case 2:
+                name = "Steve";
+                break;
+            case 3:
+                name = "Dan";
+                break;
+            case 4:
+                name = "Edna";
+                break;
+            case 5:
+                name = "Susan";
+                break;
+            case 6:
+                name = "Issac";
+                break;
+            case 7:
+                name = "Jeremey";
+                break;
+            case 8:
+                name = "Lucifer";
+                break;
+            case 9:
+                name = "Richard";
+                break;
+            case 10:
+                name = "Grayson";
+                break;
+            case 11:
+                name = "Cain";
+            killer = "your psychotic neighbor neighbor, aptly named " + name + ".";
+            break;
+        }
+    case 3:
+        killer = "a distant cousin"
 }
-var deathStory = "Long story short, you died after " + killer + death;
+var deathStory = "Long story short, you died after " + killer
