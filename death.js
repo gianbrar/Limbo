27674ff16e03bf2898d7f163d2a79fe678ee1c6c
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2020-01-26 03:51:06 -0500

    Create death.js

---
+++ death.js
@@ -0,0 +1,7 @@
+var randomVar = Math.floor((Math.random() * 50) + 1);
+var killer = "";
+var death = "";
+if (randomVar === 1) {
+    killer = "Yakuza";
+}
+var deathStory = "Long story short, you died after " + killer + death;
